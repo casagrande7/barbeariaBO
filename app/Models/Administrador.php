552c5e20ee7ee0b31b09742a89e80f067189a327
casagrande7 <joaofootz10@gmail.com>
2024-02-15 15:44:30 -0300

    at

--- app/Models/Administrador.php
+++ app/Models/Administrador.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Auth\Events\Authenticated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
